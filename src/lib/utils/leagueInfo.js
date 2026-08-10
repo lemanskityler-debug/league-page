498ba3f6aeaf64c637d6ff1a7748c0086a611bff
lemanskityler-debug <lemanskityler@gmail.com>
2026-08-10 12:21:19 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -37,7 +37,7 @@ export const managers = [
     //   "tookOver": 2020, // (DEPRECATED! You don't need to use this anymore) (optional) used if a manager took over a team, delete this line or change to null otherwise
     /   "location": "St Pete", // (optional)
     /   "bio": "Lorem ipsum...",
-    /   "photo": "IMG_1007.jpeg", // square ratio recommended (no larger than 500x500)
+    /   "photo": "managers/IMG_1007.jpeg", // square ratio recommended (no larger than 500x500)
     /   "fantasyStart": 2022, // (optional) when did the manager start playing fantasy football
     /   "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
     //   "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
